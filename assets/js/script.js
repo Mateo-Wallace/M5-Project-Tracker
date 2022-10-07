ed278d3b79248d565c379533b6b337518f807238
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-10-07 11:07:49 -0400

    added function to show Modal

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const projectButtonEl = document.getElementById('project-button');
+const modalEl = document.getElementById('modal');
+
+function showModal {
+    modalEl.classList.remove('hide')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Listening to sidebar add Project Button in order to show the Modal
+projectButtonEl.addEventListener('click', showModal);
